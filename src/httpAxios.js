62d862070fd52a07362c20479d514e6d46837649
phuongvy201 <138101347+phuongvy201@users.noreply.github.com>
2024-12-02 16:11:42 +0700

    Update httpAxios.js

--- src/httpAxios.js
+++ src/httpAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const httpAxios = axios.create({
-  baseURL: "http://localhost/hmfulfill/public/api/",
+  baseURL: "http://3.25.226.88:8000/api/",
   headers: {
     "Content-Type": "multipart/form-data",
     Accept: "application/json",
