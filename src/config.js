b32af1e1e729e1fabce5935a82ff80099a6bd7c7
phuongvy201 <138101347+phuongvy201@users.noreply.github.com>
2024-12-02 16:12:26 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const urlImage ="http://localhost/hmfulfill/public/";
+export const urlImage ="http://3.25.226.88:8000/";
 export const urlApi ="http://localhost/PhuongVy_ChuyenDeThucTap/laravelAPI/public/api/";
